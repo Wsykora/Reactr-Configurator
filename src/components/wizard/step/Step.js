35b27e2f70cc74b341e3becab61f1fc5af6e83e4
Chris Carey <crc2220@gmail.com>
2018-08-27 18:20:43 -0400

    beginnings of right side
    not sure whether or not to use react-router or try a custom solution

--- src/components/wizard/step/Step.js
+++ src/components/wizard/step/Step.js
@@ -3,9 +3,13 @@ import "./Step.css";
 
 function Step(props) {
 
+    let stepClass = props.active ? "step step-active" : "step step-deactive"
     return (
-        <div>
-            Step
+        <div className={stepClass}>
+            <h3>{props.name}</h3>
+            <p>
+                Checking for <b>node.js...</b>
+            </p>
         </div>
     );
 
